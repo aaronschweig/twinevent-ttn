d8fe322af9e44ea6216494c722aed9e50e8aee7d
Aaron Schweig <aaron.schweig@gmail.com>
2020-12-10 23:59:34 +0100

    Pushes example config

--- main.go
+++ main.go
@@ -28,22 +28,22 @@ func main() {
 	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
 
 	// Initialize Services
-	log.Info("Initialize Services...")
+	log.Info("Initializing Services...")
 	ttnService := ttn.NewTTNService(conf)
 	ms := mqtt.NewMqttService(ttnService)
 	ds := ditto.NewDittoService(conf)
 
 	// Start MQTT
-	log.Info("Connect to MQTT-Broker...")
+	log.Info("Connecting to MQTT-Broker...")
 	client := ms.Start()
 	defer client.Disconnect(250)
 
 	// Start TTN
-	log.Info("Connect to TTN...")
+	log.Info("Connecting to TTN...")
 	ttnClient := ttnService.CreateConnection()
 	defer ttnClient.Close()
 
-	log.Info("Setup Subscription...")
+	log.Info("Setting up Subscription...")
 	token := client.Subscribe("registration/+", 0b0, ms.RegistrationHandler)
 
 	if token.Wait() && token.Error() != nil {
@@ -51,7 +51,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.Info("Ensure Ditto-TTN-Connection...")
+	log.Info("Ensuring Ditto-TTN-Connection...")
 	if err = ds.CreateTTNConnection(); err != nil {
 		panic(err)
 	}
